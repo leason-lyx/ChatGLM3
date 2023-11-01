78742a32293d1fd07dfc1f1aa88879dc6d208c8f
leason-lyx <2200013104@stu.pku.edu.cn>
2023-11-02 02:15:23 +0800

    add models

--- web_demo2.py
+++ web_demo2.py
@@ -3,24 +3,26 @@ import torch
 from transformers import AutoModel, AutoTokenizer
 
 # 设置页面标题、图标和布局
-st.set_page_config(
-    page_title="ChatGLM3-6B 演示",
-    page_icon=":robot:",
-    layout="wide"
-)
+st.set_page_config(page_title="ChatGLM3-6B 演示",
+                   page_icon=":robot:",
+                   layout="wide")
 
 # 设置为模型ID或本地文件夹路径
-model_path = "THUDM/chatglm3-6b"
+model_path = "chatglm3-6b"
+
 
 @st.cache_resource
 def get_model():
-    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    model = AutoModel.from_pretrained(model_path, trust_remote_code=True).cuda()
+    tokenizer = AutoTokenizer.from_pretrained(model_path,
+                                              trust_remote_code=True)
+    model = AutoModel.from_pretrained(model_path,
+                                      trust_remote_code=True).cuda()
     # 多显卡支持,使用下面两行代替上面一行,将num_gpus改为你实际的显卡数量
     # from utils import load_model_on_gpus
     # model = load_model_on_gpus("THUDM/chatglm3-6b", num_gpus=2)
     model = model.eval()
     return tokenizer, model
+
 
 # 加载Chatglm3的model和tokenizer
 tokenizer, model = get_model()
@@ -70,14 +72,14 @@ if prompt_text:
     history = st.session_state.history
     past_key_values = st.session_state.past_key_values
     for response, history, past_key_values in model.stream_chat(
-        tokenizer,
-        prompt_text,
-        history,
-        past_key_values=past_key_values,
-        max_length=max_length,
-        top_p=top_p,
-        temperature=temperature,
-        return_past_key_values=True,
+            tokenizer,
+            prompt_text,
+            history,
+            past_key_values=past_key_values,
+            max_length=max_length,
+            top_p=top_p,
+            temperature=temperature,
+            return_past_key_values=True,
     ):
         message_placeholder.markdown(response)
 
